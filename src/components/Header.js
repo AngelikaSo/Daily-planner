940291cafaecb9852062f6dc1abb963ab1ffbfab
Angelika Sowka <sowkaangelika@gmail.com>
2024-07-18 23:48:59 -0400

    Created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Daily Planner 2024</h1>
+      <p>Week of: July 15 - July 21, 2024</p>
+    </div>
+  );
+};
+
+export default Header;
