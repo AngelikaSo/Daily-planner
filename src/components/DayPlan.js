22847db6627860c34f4927fd9a3c227c9c3a0afa
Angelika Sowka <sowkaangelika@gmail.com>
2024-08-20 22:38:48 -0400

    Daily hours array created

--- src/components/DayPlan.js
+++ src/components/DayPlan.js
@@ -1,6 +1,19 @@
 // Create a function that will display a list of hours from 6am to 10pm
 
+import { useState } from "react";
+
+const hours = [];
+for (let i = 6; i <= 22; i++) {
+  let hour = i > 12 ? i - 12 : i;
+  let period = i >= 12 ? "PM" : "AM";
+  let formatedHours = `${hour} ${period}`;
+  hours.push(formatedHours);
+}
+
+console.log(hours);
+
 const DayPlan = () => {
+  const [hourly, setHourly] = useState([]);
   return (
     <div className="day-plan">
       <h2>Today's Plan</h2>
