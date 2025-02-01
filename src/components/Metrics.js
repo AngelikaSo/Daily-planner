bb731d4f4bd4692d4e22f1789fdfcf24fb5ba6d1
Angelika Sowka <sowkaangelika@gmail.com>
2025-01-31 22:04:41 -0500

    created metrics component

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -1,30 +1,55 @@
+import { useState } from "react";
+
+import { ReactComponent as WaterDrop } from "../assets/droplet-solid.svg";
+import { ReactComponent as SmileFace } from "../assets/face-smile-regular.svg";
+import { ReactComponent as SadFace } from "../assets/face-sad-cry-regular.svg";
+import { ReactComponent as HappyFace } from "../assets/face-laugh-beam-regular.svg";
+import { ReactComponent as LoveFace } from "../assets/face-grin-hearts-regular.svg";
+import { ReactComponent as TiredFace } from "../assets/face-tired-regular.svg";
+
 const Metrics = () => {
+  const [selectedWater, setSelectedWater] = useState([]);
+  const [selectedMood, setSelectedMood] = useState([]);
+
+  const toggleSelection = (index, selected, setSelected) => {
+    if (selected.includes(index)) {
+      setSelected(selected.filter((i) => i !== index));
+    } else {
+      setSelected([...selected, index]);
+    }
+  };
+
   return (
     <div className="metrics">
-      <label>
-        Weather
-        <input className="metrics__listInput" type="text" />
-      </label>
-      <label>
-        Mood
-        <input className="metrics__listInput" type="text" />
-      </label>
-      <label>
-        Health
-        <input className="metrics__listInput" type="text" />
-      </label>
-      <label>
-        Water
-        <input className="metrics__listInput" type="text" />
-      </label>
-      <label>
-        Exercise
-        <input className="metrics__listInput" type="text" />
-      </label>
-      <label>
-        Productivity
-        <input className="metrics__listInput" type="text" />
-      </label>
+      <p>Weather</p>
+      {[...Array(5)].map((_, index) => (
+        <WaterDrop
+          className="metrics__icon"
+          key={index}
+          onClick={() =>
+            toggleSelection(index, selectedWater, setSelectedWater)
+          }
+          style={{
+            fill: selectedWater.includes(index) ? "#d89696" : "#e0e0e0",
+          }}
+        />
+      ))}
+
+      <p>Mood</p>
+      {[SmileFace, SadFace, LoveFace, HappyFace, TiredFace].map(
+        (Icon, index) => (
+          <Icon
+            className="metrics__icon metrics__icon-mood"
+            key={index}
+            onClick={() =>
+              toggleSelection(index, selectedMood, setSelectedMood)
+            }
+            style={{
+              fill: selectedMood.includes(index) ? "#d89696" : "#e0e0e0",
+            }}
+          />
+        )
+      )}
     </div>
   );
 };
